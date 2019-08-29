5e0de573491f6aa1f0a7341c0a02f1a9a02202ea
Alex Escoto <afabian.escoto@gmail.com>
2019-08-29 17:02:16 -0600

    j

--- src/app/Pages/estadisticas/estadisticas.page.ts
+++ src/app/Pages/estadisticas/estadisticas.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import { InventariosService } from '../../Services/inventarios.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-estadisticas',
@@ -10,26 +12,39 @@ export class EstadisticasPage implements OnInit {
 
   @ViewChild('barCanvas') barCanvas;
   @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('doughnutCanvasService') doughnutCanvasService;
   @ViewChild('lineCanvas') lineCanvas;
 
   barChart: any;
   doughnutChart: any;
   lineChart: any;
-  constructor() { }
+
+
+  entregasDiarias = []
+
+  constructor(private Pro_inventarios:InventariosService,
+              private storage:Storage) { }
 
   ngOnInit() {
-    this.barChartMethod();
-    this.doughnutChartMethod();
-    this.lineChartMethod();
+    this.storage.get('token').then(token=>{
+      this.Pro_inventarios.getChart(token).subscribe(data=>{
+        this.entregasDiarias = data
+        console.log(data)
+        this.barChartMethod();
+        this.doughnutChartMethod();
+        this.lineChartMethod();
+        this.doughnutChartMethodServicio();
+      })
+    })
   }
 
   barChartMethod() {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
       type: 'bar',
       data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        labels: ['LUN', 'MAR', 'MIE', 'JUE', 'VIE', 'SAB'],
         datasets: [{
-          label: '# of Votes',
+          label: '# Entregas semanal',
           data: [200, 50, 30, 15, 20, 34],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
@@ -89,14 +104,41 @@ export class EstadisticasPage implements OnInit {
     });
   }
 
+  doughnutChartMethodServicio() {
+    this.doughnutChart = new Chart(this.doughnutCanvasService.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
+    });
+  }
+
   lineChartMethod() {
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
       type: 'line',
       data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
         datasets: [
           {
-            label: 'Sell per week',
+            label: 'Entregas por mes',
             fill: false,
             lineTension: 0.1,
             backgroundColor: 'rgba(75,192,192,0.4)',
--- src/app/Services/inventarios.service.ts
+++ src/app/Services/inventarios.service.ts
@@ -29,4 +29,19 @@ export class InventariosService {
     let url = `${WEB_SERVICE}api/inventarios/insertTransaccion`
     return await this.Pro_http.post(url, body, { headers }).toPromise()
   }
+
+  getChart(p_token){
+    //Preparacion de header
+    const headers = new HttpHeaders(Header);
+
+    //Preparacion de body
+    let body = {
+      token: p_token
+    }
+
+    let url = `${WEB_SERVICE}api/inventarios/getChart`
+    return this.Pro_http.post(url, body, { headers }).pipe(map((result: any) => {
+      return result;
+    }));
+  }
 }
